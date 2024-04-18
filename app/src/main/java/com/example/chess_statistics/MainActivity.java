3ffb5235581afb7c2cef09c7846c7824ff53b2a8
Your Name <khaikaka2k2@gmail.com>
2024-04-18 20:29:10 +0700

    .

--- app/src/main/java/com/example/chess_statistics/MainActivity.java
+++ app/src/main/java/com/example/chess_statistics/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.chess_statistics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText editUsename, editPassword;
+    private Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_rating);
+        setContentView(R.layout.activity_main);
+        editUsename = findViewById(R.id.editUsername);
+        editPassword = findViewById(R.id.editPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickLogin();
+            }
+        });
     }
-}
+
+    private void clickLogin() {
+        String strUsername = editUsename.getText().toString().trim();
+        String strPassword = editPassword.getText().toString().trim();
+        User user = new User(strUsername, strPassword);
+        if(user.isValidEmail() && user.isVaildPassword()) {
+            Toast.makeText(this, "Chinh Xac", Toast.LENGTH_SHORT).show();
+//            FragmentRating fragmentRating = new FragmentRating();
+//            getSupportFragmentManager().beginTransaction()
+//                    .replace(R.layout.activity_main, fragmentRating)
+//                    .commit();
+        }
+        else {
+            Toast.makeText(this, "Khong chinh xac", Toast.LENGTH_SHORT).show();
+        }
+        }
+    }
---
+++ app/src/main/java/com/example/chess_statistics/fragment/FragmentProfile.java
@@ -0,0 +1,23 @@
+package com.example.chess_statistics.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.chess_statistics.R;
+
+public class FragmentProfile extends Fragment {
+    public FragmentProfile() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile,container,false);
+    }
+}
